a8593d71bb4cf9b81a9b8979b5071df5f7c01fae
Monisha-23 <67052670+Monisha-23@users.noreply.github.com>
2022-06-01 13:01:54 +0530

    Update settings.py

--- medicen_prj/settings.py
+++ medicen_prj/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT=os.path.normpath(os.path.join(BASE_DIR,'staticfiles'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
