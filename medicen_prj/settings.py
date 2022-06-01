9fd321df5e1dcf7d95ca69cef37adba795ace2a7
Monisha-23 <67052670+Monisha-23@users.noreply.github.com>
2022-06-01 12:17:42 +0530

    Update settings.py

--- medicen_prj/settings.py
+++ medicen_prj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+rs^_+!*-fua7n8d+=ny3ge8wnpwxrl)fmp&(v(pxgv@7e&cf_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['medicen_prj.herokuapp.com']
 
 
 # Application definition
