5c84860fcb35a4d6cc89c42f78f51fc6821be6da
Monisha-23 <67052670+Monisha-23@users.noreply.github.com>
2022-06-01 12:59:38 +0530

    Update settings.py

--- medicen_prj/settings.py
+++ medicen_prj/settings.py
@@ -124,7 +124,10 @@ import os
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.normpath(os.path.join(BASE_DIR,'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 django_heroku.settings(locals())
 
 # Default primary key field type
